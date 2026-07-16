61a784dc8bd15a04cd78abd382b4da9ee553dc69
Moorejae <your_yonkkalu@gmail.com>
2026-07-16 13:44:22 +0100

    fix: replace broken TrustWallet Monero icon URL with cryptologos

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -21,7 +21,7 @@ const DEFAULT_TOKENS: TokenItem[] = [
   { symbol: 'BTC', name: 'Bitcoin', price: 30121.75, change: 0.12, iconUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/bitcoin/info/logo.png', sparklineData: [40, 45, 42, 50, 48, 55, 60] },
   { symbol: 'ETH', name: 'Ethereum', price: 121.73, change: -0.56, iconUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png', sparklineData: [60, 55, 58, 45, 48, 40, 35] },
   { symbol: 'BNB', name: 'BNB', price: 38.88, change: -0.03, iconUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/smartchain/info/logo.png', sparklineData: [45, 48, 42, 40, 38, 42, 38] },
-  { symbol: 'XMR', name: 'Monero', price: 107.23, change: 3.45, iconUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/monero/info/logo.png', sparklineData: [20, 25, 30, 40, 50, 55, 60] },
+  { symbol: 'XMR', name: 'Monero', price: 107.23, change: 3.45, iconUrl: 'https://cryptologos.cc/logos/monero-xmr-logo.png', sparklineData: [20, 25, 30, 40, 50, 55, 60] },
   { symbol: 'USDT', name: 'Ethereum', price: 100.00, change: -3.08, iconUrl: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png', sparklineData: [50, 52, 48, 49, 45, 42, 40] },
 ];
 
